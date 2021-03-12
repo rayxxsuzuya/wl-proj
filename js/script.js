60258d71d1d05fa7f1e14f0910a77f3a784d0b61
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-03-12 20:07:05 +0300

    about & reviews adaptive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-$('.reviews__slider').slick({
+$(".reviews__slider").slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 2,
   arrows: false,
   dots: true,
+  responsive: [
+    {
+      breakpoint: 1031,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 728,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
