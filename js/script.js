70646b91dbdf13469baa471e1f8989b490edad12
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-03-13 00:52:03 +0300

    pop-up's

--- js/script.js
+++ js/script.js
@@ -23,3 +23,45 @@ $(".reviews__slider").slick({
     },
   ],
 });
+
+$(".diplomas__block").magnificPopup({
+  delegate: "a", // child items selector, by clicking on it popup will open
+  type: "image",
+  // other options
+});
+
+$(".header-main__link").magnificPopup({
+  type: "inline",
+  preloader: false,
+  focus: "#name",
+
+  // When elemened is focused, some mobile browsers in some cases zoom in
+  // It looks not nice, so we disable it:
+  callbacks: {
+    beforeOpen: function () {
+      if ($(window).width() < 700) {
+        this.st.focus = false;
+      } else {
+        this.st.focus = "#name";
+      }
+    },
+  },
+});
+
+$(".about__link").magnificPopup({
+  type: "inline",
+  preloader: false,
+  focus: "#name",
+
+  // When elemened is focused, some mobile browsers in some cases zoom in
+  // It looks not nice, so we disable it:
+  callbacks: {
+    beforeOpen: function () {
+      if ($(window).width() < 700) {
+        this.st.focus = false;
+      } else {
+        this.st.focus = "#name";
+      }
+    },
+  },
+});
